bdbd5a7273a4ec344ddc8c2dad2248535d1e0425
itchaox <1462831573@qq.com>
2023-06-18 16:56:00 +0800

    feat: Vite 基本使用

---
+++ 04_Learn_Vite/src/m.ts
@@ -0,0 +1,11 @@
+/*
+ * @Version    : v1.00
+ * @Author     : itchaox
+ * @Date       : 2023-06-18 15:46
+ * @LastAuthor : itchaox
+ * @LastTime   : 2023-06-18 15:47
+ * @desc       :
+ */
+export function sum(num1: number, num2: number): number {
+  return num1 + num2;
+}
---
+++ 04_Learn_Vite/src/math.js
@@ -0,0 +1,14 @@
+/*
+ * @Version    : v1.00
+ * @Author     : itchaox
+ * @Date       : 2023-06-18 15:32
+ * @LastAuthor : itchaox
+ * @LastTime   : 2023-06-18 16:15
+ * @desc       :
+ */
+import _ from 'lodash';
+import { sum } from './m.ts';
+
+console.log(sum(1, 2));
+
+console.log(_.join(['aa', 'bb'], '**'));
